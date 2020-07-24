647c0e6ef1c0cf16122020ab7ab7aa9c32590e42
rafaelgit85 <you@example.com>
2020-07-23 21:04:07 -0300

    Number comparator

---
+++ prjCppEstruturasCondicionais/prjCppEstruturasCondicionais/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+void main() {
+
+	int val1, val2;
+
+	cout << "\t NUMBER COMPARATOR" << endl;
+	cout << "\t-----------------" << endl;
+	cout << "Please, enter value 1: ";
+	cin >> val1;
+	cout << "Please, enter value 2: ";
+	cin >> val2;
+	cout << ""<<endl;
+	if (val1 > val2)
+	{
+		cout << "The value " << val1 << " is greater than " << val2 << ".";
+	}
+	else
+	{
+        cout<< "The value " << val1 << " is smaller than " << val2 << ".";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
